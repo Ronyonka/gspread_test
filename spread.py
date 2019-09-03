a2d970b0f7aa2bb6b7147619fd328375e925228a
Ronyonka <rononyonka@gmail.com>
2019-09-03 14:33:18 +0300

    added function to cross reference Node address with IMEI numbers from the sheet

---
+++ spread.py
@@ -0,0 +1,18 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials as SAC
+
+scope = ['https://www.googleapis.com/auth/drive']
+creds = SAC.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+sheet = client.open('TestSheet').sheet1
+everything = sheet.get_all_records()
+node = '14000A32'
+
+def printer(everything, node):
+    arr = []
+    for i in everything:
+        if i.get('NodeAddr') == node:
+            return i.get('IMEI')
+
+print(printer(everything, node))
